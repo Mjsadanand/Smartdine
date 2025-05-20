86233746af7d9977e3c4dcce713108652147dd0a
Sadanand J M <147255461+Mjsadanand@users.noreply.github.com>
2025-05-20 17:22:34 +0530

    Notify

--- backend/utils/sms.js
+++ backend/utils/sms.js
@@ -1,8 +1,12 @@
 import twilio from 'twilio';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const fromNumber = process.env.TWILIO_PHONE_NUMBER;
+
+console.log('SID:', accountSid, 'TOKEN:', authToken, 'FROM:', fromNumber);
 
 const client = twilio(accountSid, authToken);
 
